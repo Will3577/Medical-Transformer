6b011abc8a1de4d2b4a4cfe56fe42e346134ef79
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-12 20:26:59 +1100

    add unet

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -420,7 +420,7 @@ class UNet(nn.Module):
     def __init__(self, in_channels, out_channels, conv_depths=(64, 128, 256, 512, 1024)):
         assert len(conv_depths) > 2, 'conv_depths must have at least 3 members'
 
-        super(UNet2D, self).__init__()
+        super(UNet, self).__init__()
 
         # defining encoder layers
         encoder_layers = []
